3d4a3d9d7a34379871e792643b255c676ab2463e
GollyE <evan.golly@gmail.com>
2025-04-05 16:40:38 -1000

    finished HTML Node Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,3 +7,14 @@ class HTMLNode:
         self.children = children
         self.props = props
         
+    def to_html(self):
+        raise NotImplementedError("This method must be overridden by subclasses")
+    
+    def props_to_html(self):
+        attribute_string = ""
+        for key,value in self.props.items():
+            attribute_string = attribute_string+" "+key+"="+'"'+value+'"'
+        return attribute_string
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
