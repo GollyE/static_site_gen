369db625e3537e720af94f37633966fa1442ed5a
GollyE <evan.golly@gmail.com>
2025-04-06 13:53:28 -1000

    CH2 complete

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from textnode import *
 
 class HTMLNode:
 
@@ -52,6 +54,23 @@ class ParentNode(HTMLNode):
                 final_string += child_node.to_html()
         return f"<{self.tag}>{final_string}</{self.tag}>"
     
+def text_node_to_html_node(text_node):
+      match (text_node.text_type):
+        case (TextType.TEXT):
+            return LeafNode(tag=None,value=text_node.text)
+        case (TextType.BOLD):
+            return LeafNode(tag="b",value=text_node.text)
+        case (TextType.ITALIC):
+            return LeafNode(tag="i",value=text_node.text)
+        case (TextType.CODE):
+            return LeafNode(tag="code",value=text_node.text)
+        case (TextType.LINK):
+            return LeafNode(tag="a",value=text_node.text,props={"href":text_node.url})
+        case (TextType.IMAGE):
+            return LeafNode(tag="img",value="",props={"src":text_node.url,"alt":text_node.text})
+        case _:
+            raise Exception("The node type is not one of the supported node types")
+
 
 
         
