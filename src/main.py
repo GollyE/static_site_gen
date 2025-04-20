a0dad3913add9e7b6bf2ff4c9beb19632d94f842
GollyE <evan.golly@gmail.com>
2025-04-19 16:40:44 -1000

    delete public folder contents

--- src/main.py
+++ src/main.py
@@ -2,68 +2,33 @@ from textnode import *
 from htmlnode import *
 from inline import *
 from markdown_blocks import *
+import os
+import shutil
 
 
 def main():
-    test_node = TextNode("This is some anchor text",TextType.LINK,"https://www.google.com")
-    #print(test_node)
+    delete_and_copy()
 
-    test_node2=LeafNode("a", "Click me!",props = {"href":"https://www.google.com"})
-    #print(test_node2)
+def delete_contents(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)  # Delete file or symbolic link
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)  # Delete folder and its contents
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
-    test_node3 = TextNode("##bold## is the best ##bold2## is really ##bold3##",TextType.TEXT)
+def delete_and_copy():
+    delete_contents(public_folder)
+    return f"deleted the contents of {public_folder}"
 
-    old_nodes = [test_node,test_node3]
-    delimiter = "##"
-    text_type = TextType.BOLD
-    result = split_nodes_delimiter(old_nodes, delimiter, text_type)
-    #print(result)
 
-    # matches = extract_markdown_links(
-    #         "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    #     )
-    # print(matches)
+if __name__ == "__main__":
+    current_dir = os.path.dirname(__file__)
+    public_folder = os.path.normpath(os.path.join(current_dir, '..', 'public'))
 
-    # node = TextNode(
-    #     "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
-    #     TextType.TEXT,
-    # )
-
-    # node2 = TextNode(
-    #             "This is 2text with an ![2image](https://i.22imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-    #             TextType.TEXT,
-    #         )
-
-    # new_nodes = split_nodes_link([node])
-    # print(f"the new nodes are: {new_nodes}")
-
-    #text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    # md = """
-    #     This is **bolded** paragraph
-
-    #     This is another paragraph with _italic_ text and `code` here
-    #     This is the same paragraph on a new line
-
-    #     - This is a list
-    #     - with items
-    #     """
-    # blocks = markdown_to_blocks(md)
     
-    # print(f"{blocks}")
-    md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-This is another paragraph with _italic_ text and `code` here
-
-This is just a simple paragraph with no markdown and no children
-
-```
-This is a code block
-```
-
-"""  
-    markdown_to_html_node(md)
 main()
 
